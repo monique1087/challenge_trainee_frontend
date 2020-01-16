3af076996bd2b6695d4bea6b41b582d81423ddfb
moniquealmeida <monique10879@gmail.com>
2020-01-16 12:24:26 -0300

    desenvolvendo formulário e colunas da mãe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
-import Table from './components/Table';
+import Table from './components/Mae';
 
 function App() {
   return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,19 +3,22 @@ import React from 'react';
 class Form extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
+      this.state = {nome: ''};
+      this.state = {mensagem:''};
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
     handleChange(event) {
-      this.setState({value: event.target.value});
+      this.setState({nome: event.target.nome});
+      this.setState({mensagem: event.target.mensagem})
     }
   
     handleSubmit(event) {
-      alert('Uma mensagem foi enviada: ' + this.state.value);
+      alert('Uma mensagem foi enviada: ' + this.nome + '' + this.mensagem);
       event.preventDefault();
+      console.log(alert)  
     }
   
     render() {
@@ -23,9 +26,9 @@ class Form extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             Nome:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="text" value={this.props.nome} onChange={this.props.handleChange} />
             Mensagem:
-            <input type='text' value={this.state.value} onChange={this.handleChange}/>
+            <input type='text' value={this.props.mensagem} onChange={this.props.handleChange}/>
           </label>
           <input type="submit" value="Enviar" />
         </form>
